981fb0978097eb97f90f8e0befa6688b74eb039e
Anson Foong <afoong@albany.edu>
2019-10-31 12:33:35 -0400

    youtube-search bot

---
+++ youtube-search/bot.js
@@ -0,0 +1,54 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+const config = require('./config.json');
+const search = require('youtube-search');
+const opts = {
+    maxResults: 25,
+    key: config.YOUTUBE_API,
+    type: 'video'
+};
+
+client.on('ready', () => console.log("Logged in."));
+client.on('message', async message => {
+    if(message.author.bot) return;
+
+    if(message.content.toLowerCase() === '!search') {
+        let embed = new discord.RichEmbed()
+            .setColor("#73ffdc")
+            .setDescription("Please enter a search query. Remember to narrow down your search.")
+            .setTitle("YouTube Search API");
+        let embedMsg = await message.channel.send(embed);
+        let filter = m => m.author.id === message.author.id;
+        let query = await message.channel.awaitMessages(filter, { max: 1 });
+        let results = await search(query.first().content, opts).catch(err => console.log(err));
+        if(results) {
+            let youtubeResults = results.results;
+            let i  =0;
+            let titles = youtubeResults.map(result => {
+                i++;
+                return i + ") " + result.title;
+            });
+            console.log(titles);
+            message.channel.send({
+                embed: {
+                    title: 'Select which song you want by typing the number',
+                    description: titles.join("\n")
+                }
+            }).catch(err => console.log(err));
+            
+            filter = m => (m.author.id === message.author.id) && m.content >= 1 && m.content <= youtubeResults.length;
+            let collected = await message.channel.awaitMessages(filter, { maxMatches: 1 });
+            let selected = youtubeResults[collected.first().content - 1];
+
+            embed = new discord.RichEmbed()
+                .setTitle(`${selected.title}`)
+                .setURL(`${selected.link}`)
+                .setDescription(`${selected.description}`)
+                .setThumbnail(`${selected.thumbnails.default.url}`);
+
+            message.channel.send(embed);
+        }
+    }
+});
+
+client.login(config.TOKEN);
